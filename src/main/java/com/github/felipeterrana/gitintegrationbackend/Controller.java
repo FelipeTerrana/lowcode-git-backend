2ce0065fe2f4f03a256cfa68057d5e393a424863
Felipe Terrana Cazetta <felipetcazetta@gmail.com>
2023-06-22 07:24:02 -0300

    Adaptar para receber corretamente a chamada do frontend

--- src/main/java/com/github/felipeterrana/gitintegrationbackend/Controller.java
+++ src/main/java/com/github/felipeterrana/gitintegrationbackend/Controller.java
@@ -4,12 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
     @PostMapping("/commit")
-    public Map<String, Object> commit(Map<String, Object> request) {
+    @ResponseBody
+    public Map<String, Object> commit(@RequestBody Map<String, String> request) {
         // TODO
         return new HashMap<String, Object>() {
         {
