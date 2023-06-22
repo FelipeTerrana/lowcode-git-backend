d15fc6f3ba9b4f8cd286585cf1253a6ddae13c23
Felipe Terrana Cazetta <felipetcazetta@gmail.com>
2023-06-22 07:44:42 -0300

    Criar arquivo pela requisição

--- src/main/java/com/github/felipeterrana/gitintegrationbackend/Controller.java
+++ src/main/java/com/github/felipeterrana/gitintegrationbackend/Controller.java
@@ -1,5 +1,9 @@
 package com.github.felipeterrana.gitintegrationbackend;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,12 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @PostMapping("/commit")
     @ResponseBody
-    public Map<String, Object> commit(@RequestBody Map<String, String> request) {
-        // TODO
+    public Map<String, Object> commit(@RequestBody Map<String, String> request) throws IOException {
+        String path = "repository/" + request.get("path");
+        byte[] fileContent = Base64.getDecoder().decode(request.get("contentBase64"));
+
+        createFile(path, fileContent);
+
         return new HashMap<String, Object>() {
         {
             put("sucess", true);
         }
         };
+    }
+
+    private static void createFile(String path, byte[] content) throws IOException {
+        File file = new File(path);
+        file.getParentFile().mkdirs();
+        file.createNewFile();
+
+        FileOutputStream outputStream = new FileOutputStream(file);
+        outputStream.write(content);
+
+        outputStream.close();
     }
 }
